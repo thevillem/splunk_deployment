5196767584ec02249f150a26e0debb8d314f2e46
Mike Villeneuve <mike.villen@gmail.com>
2019-01-06 12:23:06 -0500

    Added rules to allow communication between EC2 instances.

--- splunk.tf
+++ splunk.tf
@@ -10,12 +10,12 @@ data "local_file" "public_ip" {
 }
 
 variable "vpc_cidr" {
-  description = "CIDR for the VPC"
+  description = "CIDR for the Splunk VPC"
   default = "192.168.0.0/16"
 }
 
 variable "splunk_subnet_cidr" {
-  description = "CIDR for the Splunk subnet"
+  description = "CIDR for the Splunk Indxr subnet"
   default = "192.168.1.0/24"
 }
 
@@ -29,17 +29,6 @@ resource "aws_vpc" "splunk-vpc" {
   }
 }
 
-# Define the Splunk subnet
-resource "aws_subnet" "splunk-subnet" {
-  vpc_id = "${aws_vpc.splunk-vpc.id}"
-  cidr_block = "${var.splunk_subnet_cidr}"
-  availability_zone = "us-east-1a"
-
-  tags {
-    Name = "Splunk Subnet"
-  }
-}
-
 # Define the internet gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.splunk-vpc.id}"
@@ -48,6 +37,18 @@ resource "aws_internet_gateway" "gw" {
     Name = "VPC IGW"
   }
 }
+
+# Define the Splunk Indexer subnet
+resource "aws_subnet" "splunk-subnet" {
+  vpc_id = "${aws_vpc.splunk-vpc.id}"
+  cidr_block = "${var.splunk_subnet_cidr}"
+  availability_zone = "us-east-1a"
+
+  tags {
+    Name = "Splunk Indxr Subnet"
+  }
+}
+
 
 # Define the route table
 resource "aws_route_table" "splunk-rt" {
@@ -69,7 +70,7 @@ resource "aws_route_table_association" "web-public-rt" {
   route_table_id = "${aws_route_table.splunk-rt.id}"
 }
 
-# Define the security group for splunk subnet
+# Define the security group for splunk subnets
 resource "aws_security_group" "sgsplunk" {
   name = "Splunk_Web Subnet"
   description = "Allow incoming HTTP connections & SSH access"
@@ -88,19 +89,62 @@ resource "aws_security_group" "sgsplunk" {
     cidr_blocks =  ["${data.local_file.public_ip.content}/32"]
   }
 
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
   vpc_id="${aws_vpc.splunk-vpc.id}"
 
   tags {
-    Name = "Splunk_Web SG"
+    Name = "Splunk_Web_and_SSH SG"
   }
+}
+
+resource "aws_security_group_rule" "allow_sg_tcp_traffic_in" {
+  from_port = 0
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.sgsplunk.id}"
+  self = true
+  to_port = 0
+  type = "ingress"
+}
+
+resource "aws_security_group_rule" "allow_sg_udp_traffic_in" {
+  from_port = 0
+  protocol = "udp"
+  security_group_id = "${aws_security_group.sgsplunk.id}"
+  self = true
+  to_port = 0
+  type = "ingress"
+}
+
+resource "aws_security_group_rule" "allow_sg_tcp_traffic_out" {
+  from_port = 0
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.sgsplunk.id}"
+  self = true
+  to_port = 0
+  type = "egress"
+}
+
+resource "aws_security_group_rule" "allow_sg_udp_traffic_out" {
+  from_port = 0
+  protocol = "udp"
+  security_group_id = "${aws_security_group.sgsplunk.id}"
+  self = true
+  to_port = 0
+  type = "egress"
 }
 
 # Create the Splunk Indexer EC2 Instance using the below
 # ami-id && instance_type
 resource "aws_instance" "splunk_indexer" {
-  ami           = "ami-b374d5a5"
+  ami           = "ami-009d6802948d06e52"
   instance_type = "t2.micro"
-  key_name = "splunk_indexer"
+  key_name = "splunk_hosts"
   subnet_id = "${aws_subnet.splunk-subnet.id}"
   vpc_security_group_ids = ["${aws_security_group.sgsplunk.id}"]
   associate_public_ip_address = true
@@ -114,3 +158,19 @@ resource "aws_instance" "splunk_indexer" {
   }
 }
 
+resource "aws_instance" "splunk_f1" {
+  ami           = "ami-009d6802948d06e52"
+  instance_type = "t2.micro"
+  key_name = "splunk_hosts"
+  subnet_id = "${aws_subnet.splunk-subnet.id}"
+  vpc_security_group_ids = ["${aws_security_group.sgsplunk.id}"]
+  associate_public_ip_address = true
+
+  tags {
+    Name = "splunk-f1"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.splunk_f1.public_ip} >> splunk_forwarder_ip_address.txt"
+  }
+}
